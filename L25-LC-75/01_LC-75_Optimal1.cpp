5d21d732767cc5d3c7e656dab516938999d94438
Nitye Jain <jnitye@gmail.com>
2026-05-14 22:31:47 +0530

    Add files via upload

---
+++ L25-LC-75/01_LC-75_Optimal1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void sortColors(vector<int> &nums)
+{
+    vector<int> sortarr(nums.size());
+    int start = 0;
+    int end = nums.size() - 1;
+    int mid = start + (end - start) / 2;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+
+        if (nums[i] == 0)
+        {
+            sortarr[start] = nums[i];
+            start++;
+        }
+
+        if (nums[i] == 2)
+        {
+            sortarr[end] = nums[i];
+            end--;
+        }
+    }
+
+    for (int i = start; i <= end; i++)
+    {
+        sortarr[i] = 1;
+    
+    }
+    nums = sortarr;
+}
+
+int main()
+{
+
+    return 0;
+}
