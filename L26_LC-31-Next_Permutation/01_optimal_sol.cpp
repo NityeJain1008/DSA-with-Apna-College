2c0fc753d63b6e01371727cfe4b0eec34e520dde
Nitye Jain <jnitye@gmail.com>
2026-06-17 21:38:08 +0530

    Add files via upload

---
+++ L26_LC-31-Next_Permutation/01_optimal_sol.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void nextPermutation(vector<int> &nums)
+{
+    int pivot = -1;
+    int n = nums.size();
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (nums[i] < nums[i + 1])
+        {
+            pivot = i;
+            break;
+        }
+    }
+
+    if (pivot != -1)
+    {
+        for (int i = n - 1; i > pivot; i--)
+        {
+            if (nums[i] > nums[pivot])
+            {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+    }
+
+    reverse(nums.begin() + pivot + 1, nums.end());
+}
+
+int main()
+{
+
+    return 0;
+}
