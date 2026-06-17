3dcdc9bafd62d6f9d34e390b9985a2a96b8e8935
Nitye Jain <jnitye@gmail.com>
2026-06-17 11:43:51 +0530

    Update 01_LC-53-Max_Subarray.cpp

--- L10-Kadanes_Algorithm/01_LC-53-Max_Subarray.cpp
+++ L10-Kadanes_Algorithm/01_LC-53-Max_Subarray.cpp
@@ -2,12 +2,10 @@
 using namespace std;
 int maxsum(vector<int> nums)
 {
-
     int currsum = 0, maxSum = INT_MIN;
 
     for (int val : nums)
     {
-
         currsum = currsum + val;
 
         if (currsum > maxSum)
@@ -24,7 +22,6 @@ int maxsum(vector<int> nums)
 
 int main()
 {
-
     vector <int> nums = {1, -2, 3, 4, 5} ; 
 
     cout << maxsum(nums) << endl ; 
