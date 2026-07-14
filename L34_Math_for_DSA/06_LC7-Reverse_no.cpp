b6c6549b22075e79942af874683b7e94c0f07c6b
Nitye Jain <jnitye@gmail.com>
2026-07-14 17:53:46 +0530

    Add files via upload

---
+++ L34_Math_for_DSA/06_LC7-Reverse_no.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int reverse(int n)
+{
+    int revnum = 0;
+
+    while (n != 0)
+    {
+        int dig = n % 10;
+
+        if (revnum > INT_MAX / 10 || revnum < INT_MIN / 10)
+        {
+            return 0;
+        }
+
+        revnum = (revnum * 10) + dig;
+        n = n / 10;
+    }
+
+    return revnum;
+}
+
+int main()
+{
+
+    return 0;
+}
