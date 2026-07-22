351151368125ea584f223a1ab5ba8e42878e1b49
Nitye Jain <jnitye@gmail.com>
2026-07-22 11:22:35 +0530

    Add files via upload

---
+++ L38-LC287-Finding_the_duplicate_number/01_sol.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int findDuplicate(vector<int> &arr)
+{
+    int slow = arr[0], fast = arr[0];
+
+    do
+    {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+
+    slow = arr[0];
+
+    while (slow != fast)
+    {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+
+    return slow;
+}
+
+int main()
+{
+
+    return 0;
+}
