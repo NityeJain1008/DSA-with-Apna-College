c29400b3f6c7f3ef245bc6fd6e2805b358a2e5d0
Nitye Jain <jnitye@gmail.com>
2026-08-29 22:44:20 +0530

    Add files via upload

---
+++ L44-Recursion3/02-LC90-Subsets2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    void getSubsets(vector<int> &nums, int i, vector<int> &ans, vector<vector<int>> &allSubsets)
+    {
+        if (i == nums.size())
+        {
+            allSubsets.push_back(ans);
+            return;
+        }
+
+        ans.push_back(nums[i]);
+        getSubsets(nums, i + 1, ans, allSubsets);
+        ans.pop_back();
+
+        int idx = i + 1;
+        while (idx < nums.size() && nums[idx] == nums[idx - 1])
+        {
+            idx++;
+        }
+
+        getSubsets(nums, idx, ans, allSubsets);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> allSubsets;
+        vector<int> ans;
+        getSubsets(nums, 0, ans, allSubsets);
+        return allSubsets;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
