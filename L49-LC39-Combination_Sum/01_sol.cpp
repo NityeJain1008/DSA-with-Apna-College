6f1162054b1c06890bbf9fe83dda8fe3381bee33
Nitye Jain <jnitye@gmail.com>
2026-09-02 22:54:31 +0530

    Add files via upload

---
+++ L49-LC39-Combination_Sum/01_sol.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+set<vector<int>> s;
+
+void getAllCombinations(vector<int> &arr, int idx, int target, vector<vector<int>> &ans, vector<int> &combin)
+{
+    if (idx == arr.size() || target < 0)
+    {
+        return;
+    }
+
+    if (target == 0)
+    {
+        if (s.find(combin) == s.end())
+        {
+            ans.push_back(combin);
+            s.insert(combin);
+        }
+        return;
+    }
+
+    combin.push_back(arr[idx]);
+
+    getAllCombinations(arr, idx + 1, target - arr[idx], ans, combin);
+    getAllCombinations(arr, idx, target - arr[idx], ans, combin);
+
+    combin.pop_back();
+
+    getAllCombinations(arr, idx + 1, target, ans, combin);
+}
+
+vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+{
+    vector<vector<int>> ans;
+    vector<int> combin;
+
+    getAllCombinations(candidates, 0, target, ans, combin);
+
+    return ans;
+}
+
+int main()
+{
+
+    return 0;
+}
